a6b439a8390f1ecd5355973f8a9dbbedcd80dba7
github username <um_2003_8@hotmail.com>
2020-06-02 17:56:42 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all #now we are directly rending to josn in the same action, we no longer need to deal with instance variables.
+    # @birds = Bird.all
+    # render plain: "Hello #{@birds[3].name}"
+    # render 'birds/index.html.erb'
+    # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+    # render json: {message: 'Hasses of data will get converted to JSON'}
+    # render json: ['As', 'well', 'as', 'arrays']
+    # render json: @birds
+    render json: {birds: birds, message: ['Hello Birds', 'Goodby Birds']}.to_json
   end
 end
