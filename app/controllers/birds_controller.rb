771e58f538ccb3ec312259391e14564905d50a61
Alouiscious Media <info@reneesoul.com>
2020-06-18 23:36:29 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,17 +1,21 @@
 class BirdsController < ApplicationController
   def index
-    # birds = Bird.all # - instance variables are not used in json
-    # @birds = Bird.all
+    birds = Bird.all # - instance variables are not used in json
+    @birds = Bird.all
+    # render plain: "Hello #{@birds[3].name}"
     # render 'birds/index.html.erb'
     # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
-    # json: { message: 'Hashes of data will get converted to JSON' }
-    # json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+    # render json: { message: 'Hashes of data will get converted to JSON' }
+    # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
     # plain: "Hello #{@birds[3].name}"
-    #  json: ['As','well','as','arrays']
-    #  json: @birds - instance variables are not used in json
+    #  render json: ['As','well','as','arrays']
+    #  render json: @birds #- instance variables are not used in json
     #  json: birds
-    # json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
-    # json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json --we dont need to_json
+    # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json 
+    #--we dont need instance methods (render json: @birds) when we use json. 
+    # use a local varible(render json: birds)
+    #to_json
   end
 end
 
